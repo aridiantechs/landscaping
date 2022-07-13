1c7b13da35fbae926346e2c83d3b06d98ff2d56c
JawadAhmed <56347584+jawad-gem@users.noreply.github.com>
2022-07-13 18:07:59 +0500

    social user

--- app/Http/Controllers/App/AuthController.php
+++ app/Http/Controllers/App/AuthController.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use App\Models\User;
 use App\Mail\EmailOtp;
 use App\Models\UserDevice;
+use Illuminate\Support\Str;
 use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 use App\Models\CompanyProfile;
