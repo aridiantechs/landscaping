12d5762f0681dda9238b51bc1243228f28d3c8a8
jawad-gem <jawadahmed1024@gmail.com>
2022-09-01 13:21:18 +0500

    active subs check

--- app/Models/User.php
+++ app/Models/User.php
@@ -93,7 +93,7 @@ class User extends Authenticatable implements MustVerifyEmail
     // active subscription
     public function activeSubscription()
     {
-        return $this->hasOne(Subscription::class,'customer_id','square_customer_id')->latest()->whereDate('end_date', '>=', now());
+        return $this->hasOne(Subscription::class,'customer_id','square_customer_id')->latest()->whereDate('end_date', '>=', now())->where('status','ACTIVE');
     }
 
     // subscriptions where 1 day have passed since the end date
