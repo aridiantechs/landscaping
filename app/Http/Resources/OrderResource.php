ad77ed9f9b96d61d556dcf023bed179d7cc8f3c2
jawad-gem <jawadahmed1024@gmail.com>
2022-09-07 15:07:32 +0500

    worker location

--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -43,26 +43,37 @@ class OrderResource extends JsonResource
         
         $location=$worker && $worker->location()->exists() ? (clone $worker)->location : [];
 
-        return [
-            'id' => $this->id,
-            'order_id' => $this->uuid,
-            'user' =>$this->when(Route::is('order.show') || Route::is('order.complete') ,new UserResource($this->user), $this->user->name),
-            'city'=> $this->city,
-            'state'=> $this->state,
-            'country'=> $this->country,
-            'lat'=>$this->lat,
-            'lng'=>$this->lng,
-            'full_address'=>$this->full_address,
-            'status' => $o_status,
-            'created_date' => $this->created_at->format('Y-m-d'),
-            'created_time' => $this->created_at->format('h:i A'),
-            'schedule_data'=>$this->schedule_data() ? $this->schedule_data() : null,
-            'area'=>$this->order_area ? $this->order_area : null,
-            'worker'=>  $this->when(auth()->user()->hasRole('endUser'), $worker),
-            'location'=>  (object)$this->when( request()->query('q')=='tracking', $location,''),
-            'enable_action' => $this->when(auth()->user()->hasRole('endUser') && $this->order_area()->exists() && $this->order_area->customer_response == 'PENDING', function () {
-                return true;
-            }),
-        ];
+        if (request()->query('q')=='tracking') {
+            $res__=[
+                'worker'=>  $this->when(auth()->user()->hasRole('endUser'), $worker),
+                'location'=>  (object)$this->when( request()->query('q')=='tracking', $location,'')
+            ];
+    
+        } else {
+            $res__=[
+                'id' => $this->id,
+                'order_id' => $this->uuid,
+                'user' =>$this->when(Route::is('order.show') || Route::is('order.complete') ,new UserResource($this->user), $this->user->name),
+                'city'=> $this->city,
+                'state'=> $this->state,
+                'country'=> $this->country,
+                'lat'=>$this->lat,
+                'lng'=>$this->lng,
+                'full_address'=>$this->full_address,
+                'status' => $o_status,
+                'created_date' => $this->created_at->format('Y-m-d'),
+                'created_time' => $this->created_at->format('h:i A'),
+                'schedule_data'=>$this->schedule_data() ? $this->schedule_data() : null,
+                'area'=>$this->order_area ? $this->order_area : null,
+                'worker'=>  $this->when(auth()->user()->hasRole('endUser'), $worker),
+                'enable_action' => $this->when(auth()->user()->hasRole('endUser') && $this->order_area()->exists() && $this->order_area->customer_response == 'PENDING', function () {
+                    return true;
+                }),
+            ];
+    
+        }
+        
+        
+        return $res__;
     }
 }
